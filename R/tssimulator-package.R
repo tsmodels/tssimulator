02ff6cdccc027d953799c36a691d14e97c0cad34
Alexios Galanos <alexios@4dscape.com>
2022-05-25 21:57:19 -0700

    Added example to the documentation

--- R/tssimulator-package.R
+++ R/tssimulator-package.R
@@ -8,6 +8,14 @@
 #' @importFrom utils tail
 #' @importFrom graphics grid
 #' @import tsmethods
+#' @examples
+#' library(magrittr)
+#' set.seed(105)
+#' sim <- initialize_simulator(rnorm(12*20, 0, 1), sampling = "months")
+#' sim <- sim %>% add_polynomial(order = 2, alpha = 0.1, beta = 0.02, l0 = 100, b0 = 0.25) %>%
+#' add_seasonal(frequency = 12, gamma = 0.01, init_harmonics = 4, init_scale = 3)
+#' # plot(sim, lwd = 2, ylab = "")
+#' # plot(sim, type = "components")
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
